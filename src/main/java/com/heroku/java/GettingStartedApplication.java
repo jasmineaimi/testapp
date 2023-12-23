7152868217680736bb271cf64b4866ef1f93d09b
cicicicira <orked0410@gmail.com>
2023-12-23 17:23:17 +0800

    last try

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -42,15 +42,15 @@ public class GettingStartedApplication {
         return "sign-in/AdminSignIn";
     }
 
-    //  @GetMapping("/signInCoordinator")
-    // public String coorsignin() {
-    //     return "sign-in/signInCoordinator";
-    // }
+     @GetMapping("/signInCoordinator")
+    public String coorsignin() {
+        return "sign-in/signInCoordinator";
+    }
 
-    //  @GetMapping("/signin")
-    // public String studentsignin() {
-    //     return "sign-in/signin";
-    // }
+     @GetMapping("/signin")
+    public String studentsignin() {
+        return "sign-in/signin";
+    }
 
     @GetMapping("/signup")
     public String about() {
