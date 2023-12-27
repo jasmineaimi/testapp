2bf40dc9eb8b658aa47f970cb41d3c0d6d7bedc4
Jasmine Aimi <2023379643@student.uitm.edu.my>
2023-12-27 13:27:31 +0800

    try commit

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -103,7 +103,7 @@ public class GettingStartedApplication {
         return "info_cat/info_unit";
     }
 
-               @GetMapping("/dashboardCoor")
+            @GetMapping("/dashboardCoor")
     public String dashboardCoor(){
         return "dashboard/dashboardCoor";
     }
